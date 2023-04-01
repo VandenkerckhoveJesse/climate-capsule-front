f32d30d279d1b4165853213ad09ade7bb20fddc7
Hadi El-Yakhni <hadiyakhni33@gmail.com>
2023-04-01 11:04:58 +0200

    add search/filter/select functionality to the sidebar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,7 +5,7 @@ const HomePage = () => {
     return (
         <div>
             <SideBar />
-            <MapContainer className="map-container" center={[51.0, 19.0]} zoom={4} maxZoom={18}>
+            <MapContainer className="map-container" center={[51.0, 19.0]} zoom={4} maxZoom={18} zoomControl={false}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
