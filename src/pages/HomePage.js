22cc779a1cb4274d705975005d5e0941707774fb
henry blackburn <henryjamesblackburn@hotmail.co.uk>
2023-04-01 09:52:44 +0200

    added code for finding current location

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,41 @@
-import { MapContainer, TileLayer, ZoomControl } from "react-leaflet";
+import { useState } from "react";
+import { MapContainer, TileLayer, useMapEvents, ZoomControl } from "react-leaflet";
 import SideBar from "../components/SideBar/SideBar";
+
+function LocationMarker() {
+  const [position, setPosition] = useState(null)
+  const map =useMapEvents({
+    click() {
+      map.locate()
+    },
+    locationfound(e) {
+      setPosition(e.latlng)
+      map.flyTo(e.latlng, map.getZoom())
+    },
+  })
+
+  // return position === null ? null : (
+  //   <Marker position={position}>
+  //     <Popup>Current Location</Popup>
+  //   </Marker>
+  // )
+}
+
 
 const HomePage = () => {
     return (
         <div>
             <SideBar />
-            <MapContainer className="map-container" center={[51.0, 19.0]} zoom={4} maxZoom={18}>
+            <MapContainer
+              className="map-container"
+              center={[51.0, 19.0]}
+              zoom={4}
+              maxZoom={18}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
+              <LocationMarker />
             </MapContainer>
         </div>
     );
