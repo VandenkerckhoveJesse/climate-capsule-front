7db2466e226ea02d29464724a0dc6bbb3797c5dc
Hadi El-Yakhni <hadiyakhni33@gmail.com>
2023-04-01 15:39:15 +0200

    basic details section

--- src/components/SideBar/PlaceDetailsView.js
+++ src/components/SideBar/PlaceDetailsView.js
@@ -9,6 +9,9 @@ const PlaceDetailsView = ({ selectedPlace, onGoBack }) => {
             </button>
             <div className={styles.title}>{selectedPlace.name}</div>
             <div className={styles.content}>{loremEpsum().slice(0, 1_200)}...</div>
+            <div className={styles.showMore}>
+                <button onClick={() => alert("show the book flipping thing")}>continue reading?</button>
+            </div>
             <div className={styles.footer}>
                 <div className={styles.date}>Jack Huffmann</div>
                 <div className={styles.author}>10-1-1982</div>
