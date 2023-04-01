81744cbfc52625a89d470928ecc09fb0cd34e824
henry blackburn <henryjamesblackburn@hotmail.co.uk>
2023-04-01 15:08:27 +0200

    marker click now sets place in sidebar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,7 +4,6 @@ import SideBar from "../components/SideBar/SideBar";
 import storyClosed from "../components/Map/storyClosed";
 import storyOpen from "../components/Map/storyOpen";
 import Locations from "./locations";
-import * as L from "leaflet";
 import { setSelectionRange } from "@testing-library/user-event/dist/utils";
 import { useSuggestions } from "../hooks/useSuggestions";
 
@@ -28,28 +27,31 @@ function LocationMarker() {
     );
 }
 
-function MultipleMarkers() {
-  const eventHandlers = useMemo(
-    () => ({
-      click() {
-        alert("Hello")
-      }
-    }),
-    [],
-  )
+function MultipleMarkers({ onMarkerClick }) {
 
   return Locations.location.map((x) => {
-    let position = L.latLng(x['lat'], x['lng'])
-    return (
-      <Marker position={position}
-        icon={storyClosed}
-        eventHandlers={eventHandlers}>
-        <Popup>Marker</Popup>
-      </Marker>
-    )
+    return <Markers place={x} markerClick={onMarkerClick} />
   })
 }
 
+function Markers({ place, markerClick })
+ {
+  const eventHandlers = useMemo(
+    () => ({
+      click() {
+        markerClick(place);
+      }
+    }),
+    [],)
+
+  return (
+    <Marker position={place}
+      icon={storyClosed}
+      eventHandlers={eventHandlers}>
+      <Popup>Marker</Popup>
+    </Marker>
+  )
+ }
 const HomePage = () => {
   const [icon, setIcon] = useState(storyClosed)
   const changeIcon = () => {
@@ -102,7 +104,7 @@ const HomePage = () => {
                 <Marker icon={icon} position={[50.62984, 4.86382]} onClick={() => changeIcon()}>
                     <Popup>A Story</Popup>
                 </Marker>
-                <MultipleMarkers />
+                <MultipleMarkers onMarkerClick={handleSuggestionSelect} />
             </MapContainer>
         </div>
     );
