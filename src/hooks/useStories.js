690245f83adc2ae893b3ea83a8ed1977bd9ef3fc
Hadi El-Yakhni <hadiyakhni33@gmail.com>
2023-04-02 01:10:14 +0200

    adding new story working - however, only locally, we're waiting for backend to integrate and test

--- src/hooks/useStories.js
+++ src/hooks/useStories.js
@@ -1,20 +1,37 @@
-import useSWR from "swr";
+import useSWR, { mutate as globalMutate } from "swr";
 
-const SERVER_IP = "http://139.162.167.224:3000";
+const SERVER_ADDRESS = "http://139.162.167.224:3000";
+
+const isFilteredStoriesKey = key => {
+    return Array.isArray(key) && key.length === 3 && key[0] === "filteredStories";
+};
 
 export const useStories = () => {
     const { data, isLoading, mutate } = useSWR("stories", () => {
-        return fetch(`${SERVER_IP}/stories`).then(res => res.json());
+        return fetch(`${SERVER_ADDRESS}/stories`).then(res => res.json());
     });
 
-    const addStory = async newStory => {};
+    const addStory = async newStory => {
+        try {
+            await mutate(oldStories => [...oldStories, newStory], { revalidate: false });
+            // await fetch(`${SERVER_ADDRESS}/stories`, {
+            //     method: "POST",
+            //     headers: { "Content-Type": "application/json" },
+            //     body: JSON.stringify(newStory),
+            // });
+            await globalMutate(isFilteredStoriesKey);
+        } catch (error) {
+            alert("error adding story");
+            mutate();
+        }
+    };
 
     return { stories: data, isLoading, addStory };
 };
 
 export const useFilteredStories = ({ place, radius }) => {
     const { data, error, mutate } = useSWR(["filteredStories", place, radius], async ([_, place, radius]) => {
-        let url = `${SERVER_IP}/stories`;
+        let url = `${SERVER_ADDRESS}/stories`;
         if (place) url += `?city=${place}&radius=${radius}`;
         return fetch(url).then(res => res.json());
     });
