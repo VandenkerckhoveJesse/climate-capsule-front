c20029fb5685dc6db8ab06e9f6e0074ba61eab3b
Hadi El-Yakhni <hadiyakhni33@gmail.com>
2023-04-01 12:02:11 +0200

    add zoom control at position bottom-right to not conflict with the side panel

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,47 +4,46 @@ import SideBar from "../components/SideBar/SideBar";
 import storyClosed from "../components/Map/storyClosed";
 
 function LocationMarker() {
-  const [position, setPosition] = useState(null)
-  const map =useMapEvents({
-    click() {
-      map.locate()
-    },
-    locationfound(e) {
-      setPosition(e.latlng)
-      map.setZoom(13)
-      map.flyTo(e.latlng)
-    },
-  })
+    const [position, setPosition] = useState(null);
+    const map = useMapEvents({
+        click() {
+            map.locate();
+        },
+        locationfound(e) {
+            setPosition(e.latlng);
+            map.setZoom(13);
+            map.flyTo(e.latlng);
+        },
+    });
 
-  return position === null ? null : (
-    <Marker position={position}>
-      <Popup>Current Location</Popup>
-    </Marker>
-  )
+    return position === null ? null : (
+        <Marker position={position}>
+            <Popup>Current Location</Popup>
+        </Marker>
+    );
 }
 
-const handleClick = () => {
-
-}
+const handleClick = () => {};
 
 const HomePage = () => {
     return (
         <div>
             <SideBar />
             <MapContainer
-              className="map-container"
-              center={[50.8476, 4.3572]}
-              zoom={8}
-              scrollWheelZoom={false}
-              zoomControl={false}>
+                className="map-container"
+                center={[50.8476, 4.3572]}
+                zoom={8}
+                scrollWheelZoom={false}
+                zoomControl={false}>
+                <ZoomControl position="bottomright" />
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
-              <LocationMarker />
-              <Marker icon={storyClosed} position={[50.629840, 4.863820]} onClick={handleClick}>
-                <Popup>A Story</Popup>
-              </Marker>
+                <LocationMarker />
+                <Marker icon={storyClosed} position={[50.62984, 4.86382]} onClick={handleClick}>
+                    <Popup>A Story</Popup>
+                </Marker>
             </MapContainer>
         </div>
     );
