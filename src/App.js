68019172b1dd56992ddd9a38b02644b3bd252649
Hadi El-Yakhni <hadiyakhni33@gmail.com>
2023-04-01 02:36:31 +0200

    working map - just for testing purposes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 
 function App() {
     const handleSubmit = event => {
@@ -7,16 +8,27 @@ function App() {
         }
     }
 
-  return (
-    <div className="App">
-      <h1>home page</h1>
-      <div>
-        {/*  write a basic search field */}
-        <input type="text" placeholder="search" onKeyDown={handleSubmit} />
-        <h1>map container</h1>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>home page</h1>
+            <div>
+                {/*  write a basic search field */}
+                <input type="text" placeholder="search" onKeyDown={handleSubmit}/>
+                <h1>map container</h1>
+                <MapContainer
+                    className="markercluster-map"
+                    center={[51.0, 19.0]}
+                    zoom={4}
+                    maxZoom={18}
+                >
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                </MapContainer>
+            </div>
+        </div>
+    );
 }
 
 export default App;
