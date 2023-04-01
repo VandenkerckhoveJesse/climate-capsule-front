0a30c2e3a9e4fed89aadbbd2a7ee51c47f28b5f7
Hadi El-Yakhni <hadiyakhni33@gmail.com>
2023-04-01 04:50:49 +0200

    add prettier for consistent code formatting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import './App.css';
-import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import "./App.css";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 function App() {
     const handleSubmit = event => {
-        if (event.key === 'Enter') {
+        if (event.key === "Enter") {
             alert(event.target.value);
         }
-    }
+    };
 
     return (
         <div className="App">
             <h1>home page</h1>
             <div>
-                {/*  write a basic search field */}
-                <input type="text" placeholder="search" onKeyDown={handleSubmit}/>
+                <input type="text" placeholder="search" onKeyDown={handleSubmit} />
                 <h1>map container</h1>
-                <MapContainer
-                    className="markercluster-map"
-                    center={[51.0, 19.0]}
-                    zoom={4}
-                    maxZoom={18}
-                >
+                <MapContainer className="map-container" center={[51.0, 19.0]} zoom={4} maxZoom={18}>
                     <TileLayer
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
